19e9a549f5bc7f2824fecbd3c4f4939110bbf085
Dominick <dominick@zytronic.com>
2026-01-25 11:29:27 -0500

    Updating quine example

--- c-quine-example.c
+++ c-quine-example.c
@@ -3,21 +3,3 @@
 char*s="#include <stdio.h>%cchar*s=%c%s%c;%cint main(void){printf(s,10,34,s,34,10,10);}%c";
 int main(void){printf(s,10,34,s,34,10,10);}
 
-
-
-/*
-*
-*
-* trigger code
-* char *payload = trigger + %cchar *payload = %c%s%c + code_backdoor %c"
-* backdoor code
-*
-*
-*
-*
-*
-*
-*/
-
-
-
